fa82b2dd58a6f5ada4d7ab6d5860ff3e9c9a6173
IgnetStudio <j.ignatowicz@wit.edu.pl>
2017-11-05 21:31:45 +0100

    canvas created

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+//
+// SVG
+//
+
 var svgCircle = document.querySelector("circle");
+
 svgCircle.setAttribute("fill", "#EBCB8B");
+
+//
+// CANVAS
+//
+
+var canvas = document.querySelector("canvas");
+var context = canvas.getContext("2d");
+
+context.fillStyle = "#A3BE8C";
+context.fillRect(105, 10, 90, 150);
