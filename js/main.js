2429064fe5affb625d756f61f78b38789f6e59f8
IgnetStudio <j.ignatowicz@wit.edu.pl>
2017-11-05 21:50:56 +0100

    canvas lines created

--- js/main.js
+++ js/main.js
@@ -10,10 +10,18 @@ svgCircle.setAttribute("fill", "#EBCB8B");
 // CANVAS
 //
 
-var cx = document.querySelector("canvas").getContext("2d");
+var canvaSquare = document.querySelector(".canvaSquare").getContext("2d");
+var canvaLines = document.querySelector(".canvaLines").getContext("2d");
 
-cx.fillStyle = "#A3BE8C";
-cx.fillRect(105, 10, 90, 90);
-cx.strokeStyle = "#5E81AC";
-cx.lineWidth = 3;
-cx.strokeRect(105, 10, 90, 90);
+canvaSquare.fillStyle = "#A3BE8C";
+canvaSquare.fillRect(105, 10, 90, 90);
+canvaSquare.strokeStyle = "#5E81AC";
+canvaSquare.lineWidth = 3;
+canvaSquare.strokeRect(105, 10, 90, 90);
+
+canvaLines.beginPath();
+for (var y = 10; y < 100; y += 10) {
+  canvaLines.moveTo(105, y);
+  canvaLines.lineTo(195, y);
+}
+canvaLines.stroke();
