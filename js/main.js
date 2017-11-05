9f7bfbceb1d79879d9e21d33c4155030d4866bd8
IgnetStudio <j.ignatowicz@wit.edu.pl>
2017-11-05 21:42:28 +0100

    canvas styled

--- js/main.js
+++ js/main.js
@@ -10,8 +10,10 @@ svgCircle.setAttribute("fill", "#EBCB8B");
 // CANVAS
 //
 
-var canvas = document.querySelector("canvas");
-var context = canvas.getContext("2d");
+var cx = document.querySelector("canvas").getContext("2d");
 
-context.fillStyle = "#A3BE8C";
-context.fillRect(105, 10, 90, 150);
+cx.fillStyle = "#A3BE8C";
+cx.fillRect(105, 10, 90, 90);
+cx.strokeStyle = "#5E81AC";
+cx.lineWidth = 3;
+cx.strokeRect(105, 10, 90, 90);
