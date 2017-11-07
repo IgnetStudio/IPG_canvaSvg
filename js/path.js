76eef9fcaf1c9912fc4c62c824a963c6638eb7f1
IgnetStudio <j.ignatowicz@wit.edu.pl>
2017-11-08 00:42:49 +0100

    svg path js animated

--- js/path.js
+++ js/path.js
@@ -1,1 +1,9 @@
-// placeholder
+var path = document.querySelector('.svgFire path');
+var length = path.getTotalLength();
+
+path.style.strokeDasharray = length + ' ' + length;
+path.style.strokeDashoffset = length;
+
+path.getBoundingClientRect();
+path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 4s ease-in-out';
+path.style.strokeDashoffset = '5';
